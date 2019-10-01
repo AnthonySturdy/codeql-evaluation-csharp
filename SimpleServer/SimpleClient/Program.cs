9694c90a88ff5ed0b087a1ee64bf061182a6c258
Anthony Sturdy <anthonysturdy123@gmail.com>
2019-10-01 15:21:02 +0100

    Tutorial 1 finished
    Can communicate between server and client

--- SimpleServer/SimpleClient/Program.cs
+++ SimpleServer/SimpleClient/Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 namespace SimpleClient {
     class Program {
         static void Main(string[] args) {
+            SimpleClient client = new SimpleClient();
+
+            if(client.Connect("127.0.0.1", 4444)) {
+                Console.WriteLine("Connected");
+                client.Run();
+            } else {
+                Console.WriteLine("Failed to connect to the server");
+            }
         }
     }
 }
