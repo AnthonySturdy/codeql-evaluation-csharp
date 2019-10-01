268bb889c432818c977520fd2631b434e1aa2f0b
Anthony Sturdy <35777485+AnthonySturdy@users.noreply.github.com>
2019-10-01 10:57:04 +0100

    Initial upload

---
+++ SimpleServer/SimpleClient/SimpleClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleClient {
+    class SimpleClient {
+        TcpClient client;
+        NetworkStream stream;
+        StreamWriter writer;
+        StreamReader reader;
+
+        public SimpleClient() {
+            client = new TcpClient();
+        }
+
+        public bool Connect(string ipAddress, int port) {
+            try {
+                client.Connect(IPAddress.Parse(ipAddress), port);
+                stream = client.GetStream();
+                writer = new StreamWriter(stream);
+                reader = new StreamReader(stream); 
+            } catch (Exception e) {
+                Console.WriteLine("Exception: " + e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Run() {
+            string userInput;
+
+            ProcessServerResponse();
+
+            while ((userInput = Console.ReadLine()) != null) {
+                writer.WriteLine(userInput);
+                writer.Flush();
+
+                ProcessServerResponse();
+
+                if (userInput == "Exit") {
+                    break;
+                }
+            }
+
+            client.Close();
+        }
+
+        void ProcessServerResponse() {
+            //GOT TO HERE ON THE TUTORIAL
+        }
+    }
+}
---
+++ SimpleServer/SimpleServer/SimpleServer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleServer {
+    class SimpleServer {
+        TcpListener listener;
+
+        public SimpleServer(string ipAddress, int port) {
+            listener = new TcpListener(IPAddress.Parse(ipAddress), port);
+        }
+
+        public void Start() {
+            listener.Start();
+
+            Socket socket = listener.AcceptSocket();
+
+            Console.WriteLine("Socket accepted");
+
+            SocketMethod(socket);
+        }
+
+        public void Stop() {
+            listener.Stop();
+        }
+
+        void SocketMethod(Socket socket) {
+            string receivedMessage;
+
+            NetworkStream stream = new NetworkStream(socket);
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+
+            while((receivedMessage = reader.ReadLine()) != null) {
+                string returnMsg = GetReturnMessage(receivedMessage);
+
+                writer.WriteLine(returnMsg);
+                writer.Flush();
+            }
+
+            socket.Close();
+        }
+
+        string GetReturnMessage(string code) {
+            if(code == "Hi") {
+                return "Hello";
+            } else if (code == "Joke") {
+                return "What do you call a zoo with only one dog? A shih-tzu.";
+            } else {
+                return "I don't know what to say to that.";
+            }
+        }
+
+    }
+}
