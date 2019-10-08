0723d8f3ffb319094fe83345b3140a8061f932fa
Anthony Sturdy <35777485+AnthonySturdy@users.noreply.github.com>
2019-10-08 10:40:04 +0100

    Multithreading excersise 1.1

---
+++ Week3_Threading1/Week3_Threading1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Week3_Threading1 {
+    public class ThreadWork {
+        public static void DoWork() {
+            for(int i = 100; i > 0; i--) {
+                Console.WriteLine(i + "*");
+            }
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Thread thread1 = new Thread(ThreadWork.DoWork); // NEED TO DO EXCERSIZE 1.2
+            thread1.Start();
+
+            for(int i = 0; i < 100; i++) {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
